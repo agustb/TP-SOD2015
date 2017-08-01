6d9d901affc74cf8982a1e453bb4ac87099fe96c
Agustín Tibaldo <agustin.tibaldo@gmail.com>
2017-08-01 19:38:24 -0300

    ..

--- java/tp1/materoserver/src/Cliente.java
+++ java/tp1/materoserver/src/Cliente.java
@@ -1,30 +1,37 @@
-import java.io.*;
 import java.net.*;
+import java.io.*;
 
 public class Cliente {
 	
 	private static String HOST = "localhost";
 	private static int PUERTO = 2017;
 	
-	public static void main (String args[]) {
-		Socket socket;
-		DataOutputStream mensaje;
+	private static boolean parseParameters(String args[]){
 		
+		
+			
+		
+		return false;
+	}
+	
+	
+	public static void main (String args[]) {
+		 
 		try{
 			// Creo socket
-			socket = new Socket(HOST,PUERTO);
-			mensaje = new DataOutputStream(socket.getOutputStream());
+			Socket  socket = new Socket(HOST,PUERTO);
+			DataOutputStream mensaje = new DataOutputStream(socket.getOutputStream());
 			
 			// Envio mensaje
-			mensaje = writeUTF("Hola soy un cliente!");
+			mensaje.writeUTF("Hola soy un cliente!");
 			
 			// Cerramos la conexion
 			socket.close();
 			
 		} catch (UnknownHostException e){
-			System.out.println("El host no existe o no esta activo.");
+			System.out.println("El host no existe o no esta activo. "+e.getMessage());
 		} catch (IOException e){
-			System.out.println("Error de entrada/salida.");
+			System.out.println("Error de entrada/salida. "+e.getMessage());
 		}		
 	}
 
