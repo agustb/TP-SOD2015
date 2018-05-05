9668335c9b2e437db0d18c46a4bf57719b58688e
agus_tib <agustin.tibaldo@gmail.com>
2018-05-05 16:12:55 -0300

    Server

--- java/tp2/src/server.java
+++ java/tp2/src/server.java
@@ -3,11 +3,30 @@ import java.io.*;
 import java.net.*;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.xmlrpc.*;
+import org.apache.xmlrpc.webserver.WebServer;
 
 public class server {
 
 	static final String ALUMNO = "A";
 	static final String PROFESOR = "P"; 
+	
+	static Map<String, String> map = new HashMap<String, String>();	
+	
+	// GET: devuelvo nota de libreta
+	public String get(String LU){  
+		return map.get(LU);
+	}	
+	
+	// ADD: inserto nota asociada a LU
+	public String add(String LU, String nota){
+		try {
+			map.put(LU, nota);
+			return "0";
+		} catch (Exception e) {
+			return "1";
+		}
+	}	
 	
 	public static void main(String[] args)  throws IOException {
 		int puerto = 0;
@@ -24,16 +43,20 @@ public class server {
 			}
 		}
 		/////////////////////////
-		
-		Map<String, String> map = new HashMap<String, String>();	
 		map.put("100", "9");
 		map.put("200", "4");
-		map.put("300", "7");
-
-		String linea = null;
+		map.put("300", "7");	
 		
-
-			
+		try {
+	        System.out.println("Intentando conectar al servidor RPC en puerto "+ puerto);
+	        
+	        WebServer server = new WebServer(80);
+	        server.addHandler("matero", new server());
+	        server.start();
+	        
+	        System.out.println("Conexión exitosa!");
+	        System.out.println("Escuchando peticiones (terminar programa para detener)");
+						
 		} catch (IOException e) {
 			 System.out.println("IOException: " + e.getMessage());	
 		}
